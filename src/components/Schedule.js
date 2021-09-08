d0592287cc3a1b103bf284c558fcbbb758e03b06
Nathan Kruckenberg <natekruck1990@gmail.com>
2021-09-07 19:27:18 -0500

    fixed navbar css, last thing is making the navbars z-index work properly since it was pre-set through bootstrap

---
+++ src/components/Schedule.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header.js';
+import '../css/Schedule.css';
+
+const Schedule = () => {
+    return (
+        <>
+                <Header />
+                <div className="schedule-container">
+                    <div className="schedule-header">
+                        <h1>Schedule</h1>
+                        <p>Here's what to expect during our wedding weekend. There will also be a printout of this schedule available in your hotel rooms. We can't wait to celebrate with you!</p>
+                    </div>
+                </div>
+                
+        </>
+    )
+}
+
+export default Schedule;
