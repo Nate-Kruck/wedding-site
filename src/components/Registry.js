93cd1a9a9a3caf2b13f3f33411e597702c295040
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-03-23 21:05:34 -0500

    added registry info

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -2,22 +2,49 @@ import React from 'react';
 import Header from '../components/Header.js';
 import '../css/Registry.css';
 import '../css/Queries.css';
+import crateAndBarrel from '../images/crate-barrel-logo.png';
+import bedBathBeyondImage from '../images/bed-bath-beyond.png';
+import honeyFundImage from '../images/honey-fund.png';
 
-const Registry = () => {
+class Registry extends React.Component {
+
+crateAndBarrelLink() {
+  window.open("https://www.crateandbarrel.com/gift-registry/willow-garcia-and-nathan-kruckenberg/r6483245")
+}
+
+bedBathBeyond() {
+  window.open("https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/551306537?selectedRLVFilters=_recommended")
+}
+
+honeyFund() {
+  window.open("https://www.honeyfund.com/wedding/garcia-kruckenberg-07-22-2022")
+}
+
+  render() {
     return (
-        <>
-                <Header />
-                <div className="registry-container">
-                        <h1 className="registry-message">Registry 
-                        <br/>
-                        <svg width="100%" height="100%">
-                            <text x="50%" y="70%" textAnchor="middle">Coming Soon</text>
-                        </svg>
-                        </h1>
-                        <p>Check back in March </p>
-                </div>     
-        </>
+      <div>
+        <Header />
+          <h1 className='registry-header'>Registry Links</h1>
+          <div className="registry-container">
+            <div className='registry-links'>
+             <img src={crateAndBarrel}
+                  className="the-barrel"
+                  alt="crate barrel"
+                  onClick={this.crateAndBarrelLink}>
+            </img>
+            <img src={bedBathBeyondImage}
+                alt="bed beyond"
+                onClick={this.bedBathBeyond}>
+            </img>
+            <img src={honeyFundImage}
+                alt="bed beyond"
+                onClick={this.honeyFund}>
+            </img>
+          </div>
+        </div> 
+      </div>    
     )
+  }
 }
 
 export default Registry;
